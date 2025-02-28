19ecfc569de0135663e15458987c0ae63557e056
MateoAlarcon <jmalarcon@ucundinamarca.edu.co>
2025-02-28 16:44:19 -0500

    calculo masa corporal

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,47 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Bienvanido al juego");
-        System.out.println("Adivina el numero");
+        double peso = 0;
+        double estatura = 0;
+        double IMC = 0;
 
-        int max = 50;
-        int min = 1;
-        int num;
-        int intentos = 3;
-        int unum;
+        Scanner scaner = new Scanner(System.in);
 
-        Random numAle = new Random();
-        num=numAle.nextInt(max - min + 1) + min;
-        System.out.println("escribe un numero entre 1 y 50");
-        for (int i=0 ;i<intentos;){
+        System.out.println("ingresa tu peso");
+        peso = scaner.nextDouble();
 
-            Scanner scanner = new Scanner(System.in);
-            unum=scanner.nextInt();
+        System.out.println("ingresa tu estatura");
+        estatura = scaner.nextDouble();
 
-            if (unum > num){
-                System.out.println("el numero es menor");
-                intentos--;
-            } else if (unum < num) {
-                System.out.println("el numero es mayor");
-                intentos--;
-            }else {
-                System.out.println("el numero es correcto");
-            }
+        IMC = peso/(estatura*estatura);
+        System.out.printf("Tu imc es de: " + IMC);
+        System.out.println(" ");
 
+        if(IMC < 18.5) {
+            System.out.println("tienes bajo peso");
         }
-        System.out.println("te quedaste sin intentos, el numero era: " + num);
+        else if(IMC >= 18.5 & IMC <= 24.9) {
+            System.out.println("tienes peso normal");
+        }
+        else if(IMC >= 25 & IMC <= 26.9) {
+            System.out.println("tienes sobrepeso grado 1");
+        }
+        else if(IMC >= 27 & IMC <= 29.9) {
+            System.out.println("tienes sobrepeso grado 2");
+        }
+        else if(IMC >= 30 & IMC <= 34.9) {
+            System.out.println("tienes obesidad de tipo 1");
+        }
+        else if(IMC >= 35 & IMC <= 39.9) {
+            System.out.println("tienes obesidad de tipo 2");
+        }
+        else if(IMC >= 40 & IMC <= 49.9) {
+            System.out.println("tienes obesidad de tipo 3(mórbida)");
+        }
+        else {
+            System.out.println("tienes obesidad de tipo 4(extrema)");
+        }
+    }
 }
-}
-
